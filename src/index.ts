4bb0ff6f8b60a30cfc54a05bc0ef9e1856eff94b
Joseph Chingalo <profschingalo@gmail.com>
2021-11-02 13:50:46 +0300

    feat: adding initial support for the app process

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,29 @@ import { ExcelUtil } from './utils/excel-util';
 import { LogsUtil } from './utils/logs-util';
 
 import * as _ from 'lodash';
+import { AppProcess } from './app/app-process';
 
 starApp();
 
 async function starApp() {
   try {
+    const appProcess =  new AppProcess();
+    console.log({appProcess})
     const logsUtil = new LogsUtil();
     await logsUtil.clearLogs();
     await logsUtil.addLogs('info', 'start an app', 'app');
+  
     // @TODO adding logics for the app script
-    const excelUtil = new ExcelUtil('task_list');
-    const data = await excelUtil.getJsonDataFromExcelOrCsvFile();
-    for (var key of _.keys(data)) {
-      await new ExcelUtil(`task_list-${key}`).writeToSingleSheetExcelFile(
-        _.chunk(data[key], 10)[0]
-      );
-    }
-    await new ExcelUtil('task_list-new').writeToMultipleSheetExcelFile(data);
+    // const excelUtil = new ExcelUtil('task_list');
+    // const data = await excelUtil.getJsonDataFromExcelOrCsvFile();
+    // for (var key of _.keys(data)) {
+    //   await new ExcelUtil(`task_list-${key}`).writeToSingleSheetExcelFile(
+    //     _.chunk(data[key], 10)[0]
+    //   );
+    // }
+    // await new ExcelUtil('task_list-new').writeToMultipleSheetExcelFile(data);
 
-    await logsUtil.addLogs('info', 'End of script', 'app');
+    // await logsUtil.addLogs('info', 'End of script', 'app');
   } catch (error: any) {
     error = error.message || error;
     console.log({ error });
