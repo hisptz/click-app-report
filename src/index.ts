12924c4a451be52cbb386a751bd2c8558293b50e
Joseph Chingalo <profschingalo@gmail.com>
2021-11-02 13:40:23 +0300

    feat: link up the exel util files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { ExcelUtil } from "./utils/excel-util";
 import { LogsUtil } from "./utils/logs-util";
+
+import * as _ from "lodash";
 
 starApp();
 
@@ -8,6 +11,16 @@ async function starApp() {
     await logsUtil.clearLogs();
     await logsUtil.addLogs("info", "start an app", "app");
     // @TODO adding logics for the app script
+    const excelUtil = new ExcelUtil("task_list");
+    const data = await excelUtil.getJsonDataFromExcelOrCsvFile();
+    for(var key of _.keys(data)){
+      await new ExcelUtil(
+        `task_list-${key}`
+      ).writeToSingleSheetExcelFile(_.chunk(data[key], 10)[0]);
+    }
+    await new ExcelUtil(
+      "task_list-new"
+    ).writeToMultipleSheetExcelFile(data);
 
     await logsUtil.addLogs("info", "End of script", "app");
   } catch (error: any) {
