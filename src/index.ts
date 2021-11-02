1d93c85782339fcfb981a443b1f692ca08480799
Joseph Chingalo <profschingalo@gmail.com>
2021-11-02 12:05:51 +0300

    feat: support for util file manipulation file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { FileUtil } from "./utils/file-util";
 
-const fileUtil = new FileUtil("resources/logs");
-console.log(fileUtil);
+starApp();
+
+async function starApp() {
+  const fileUtil = new FileUtil("resources/logs", "logs jose");
+
+  fileUtil.writeToFile({ id: "id" }, true);
+  const data = await fileUtil.readDataFromFile();
+  console.log(data);
+}
--- src/utils/file-util.ts
+++ src/utils/file-util.ts
@@ -1,20 +1,82 @@
 import shelljs from "shelljs";
+import fs from "fs";
+import * as _ from "lodash";
 
-export class FileUtil{
-    fileDir : string;
-    constructor(outputDir : string){
-        this.fileDir = `${outputDir}`;
-        this.intiateFilesDirectories(this.fileDir);
-    }
+export class FileUtil {
+  fileDir: string;
+  filename: string;
+  constructor(outputDir: string, filename: string) {
+    this.fileDir = `${outputDir}`;
+    this.filename = filename;
+    this.intiateFilesDirectories(this.fileDir);
+  }
 
-    intiateFilesDirectories(directories :any) {
-        return new Promise((resolve, reject) => {
-          const response = shelljs.mkdir("-p", directories);
-          if (response && !response.stderr) {
-            resolve([]);
-          } else {
-            reject(response.stderr);
-          }
-        });
+  get formattedFileName(): string {
+    return _.join(
+      _.filter(
+        _.join(
+          _.filter(this.filename.split("/"), (key) => key.trim() !== ""),
+          "_"
+        ).split(" "),
+        (key) => key.trim() !== ""
+      ),
+      "_"
+    );
+  }
+
+  getFilesNamesUsingPath(path: string) {
+    const fileResponse = shelljs.ls(`${path}`);
+    return _.flatMapDeep(
+      _.map(
+        _.filter(fileResponse.stdout.split(`\n`), (name) => name !== ""),
+        (name: string) => name.trim()
+      )
+    );
+  }
+
+  intiateFilesDirectories(directories: any) {
+    return new Promise((resolve, reject) => {
+      const response = shelljs.mkdir("-p", directories);
+      if (response && !response.stderr) {
+        resolve([]);
+      } else {
+        reject(response.stderr);
       }
+    });
+  }
+
+  async writeToFile(data: any, shouldStringify = true, flag = "w") {
+    return new Promise((resolve, reject) => {
+      data = shouldStringify ? JSON.stringify(data) : data;
+      fs.writeFile(
+        `${this.fileDir}/${this.formattedFileName}.txt`,
+        data,
+        { flag },
+        async (error) => {
+          if (error) {
+            console.log(JSON.stringify({ error, type: "writeToFile" }));
+          } else {
+            resolve([]);
+          }
+        }
+      );
+    });
+  }
+
+  async readDataFromFile() {
+    let data: any = [];
+    return new Promise((resolve) => {
+      fs.readFile(
+        `${this.fileDir}/${this.formattedFileName}.txt`,
+        (error, response: any) => {
+          if (error) {
+            console.log(JSON.stringify({ error, type: "readDataFromFile" }));
+          } else {
+            data = JSON.parse(response);
+          }
+          resolve(data);
+        }
+      );
+    });
+  }
 }
