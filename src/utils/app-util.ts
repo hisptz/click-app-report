d81f93fe9091b3c8d3c5a1c3d26a74bd447ea873
Joseph Chingalo <profschingalo@gmail.com>
2021-11-03 11:37:29 +0300

    fix: fixing period utils for date generations

--- src/utils/app-util.ts
+++ src/utils/app-util.ts
@@ -6,14 +6,16 @@ export class AppUtil {
         const dateArray = date.split('/');
         if (dateArray.length > 2) {
           const year = new Date().getFullYear();
-          const month = parseInt(dateArray[1], 10);
-          const day = parseInt(dateArray[0], 10);
+          const month = parseInt(dateArray[0], 10);
+          const day = parseInt(dateArray[1], 10);
           const newDate =
             year +
             (month > 9 ? `-${month}` : `-0${month}`) +
             (day > 9 ? `-${day}` : `-0${day}`);
           taskDate = this.getFormattedDate(newDate);
         }
+      } else if (typeof date == 'string' && date.includes('Today')) {
+        taskDate = this.getFormattedDate(reportGeneratedDate);
       } else if (typeof date == 'string' && date.includes('days ago')) {
         const numberOfdays = parseInt(date.split(' ')[0] ?? '0', 10);
         taskDate = this.getFormattedDate(
@@ -23,20 +25,30 @@ export class AppUtil {
             )
           )
         );
-      } else if (!isNaN(new Date(date).getDate())) {
-        taskDate = this.getFormattedDate(date);
       } else {
         taskDate = this.getExcelDateToJSDate(date);
       }
     } catch (error) {}
-    console.log({ taskDate, date, reportGeneratedDate });
+
+    console.log({ taskDate, date });
     return taskDate;
   }
 
   static getExcelDateToJSDate(date: any) {
-    return this.getFormattedDate(
+    let excelDate = '';
+    const dateArray = this.getFormattedDate(
       new Date(Math.round((date - 25569) * 86400 * 1000))
-    );
+    ).split('-');
+    if (dateArray.length > 2) {
+      const year = parseInt(dateArray[0], 10);
+      const month = parseInt(dateArray[2], 10);
+      const day = parseInt(dateArray[1], 10);
+      excelDate =
+        year +
+        (month > 9 ? `-${month}` : `-0${month}`) +
+        (day > 9 ? `-${day}` : `-0${day}`);
+    }
+    return excelDate;
   }
 
   static getFormattedDate(date: any) {
