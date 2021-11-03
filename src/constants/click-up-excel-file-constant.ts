edc690e1dbc258108690458e77f569be103e2b06
Joseph Chingalo <profschingalo@gmail.com>
2021-11-03 12:18:46 +0300

    feat: support for report status references

--- src/constants/click-up-excel-file-constant.ts
+++ src/constants/click-up-excel-file-constant.ts
@@ -5,8 +5,12 @@ export const createdDateColumn = 'Date created';
 export const dateClosedColumn = 'Date closed';
 export const lastUpdatedDateColumn = 'Date updated';
 export const completedDateColum = 'Task Completion Date';
-
-export const DATE_FIELD_COLUMS = [
+export const reviewStatus = 'review';
+export const closedStatus = 'Closed';
+export const openStatus = 'Open';
+export const inProgressStatus = 'in progress';
+export const taskClosedStatus = [closedStatus, reviewStatus];
+export const dateFielsColumns = [
   dueDateColumn,
   createdDateColumn,
   lastUpdatedDateColumn,
