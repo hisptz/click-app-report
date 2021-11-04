a3dff168d9f6209f2f57eba853d74eb1963ce909
Joseph Chingalo <profschingalo@gmail.com>
2021-11-04 17:20:24 +0300

    feat: sUpport for api integration configurations

---
+++ src/configs/api-config.example.ts
@@ -0,0 +1,7 @@
+import { ApiConfigModel } from '../models/api-config-model';
+
+export const apiConfig: ApiConfigModel = {
+  teamId: 'team-id-from-click-up',
+  authorizationKey: 'click-up-generated-key',
+  appBaseUrl: 'https://api.clickup.com/api/v2'
+};
