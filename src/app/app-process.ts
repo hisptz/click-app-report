66178a4bb9597c90993ccd76b18410298b2d3a4b
Joseph Chingalo <profschingalo@gmail.com>
2025-05-13 12:59:56 +0300

    feat: set support for get user reports and initiate process for tasks

--- src/app/app-process.ts
+++ src/app/app-process.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import { LogsUtil } from '../utils';
-import { ProjectProcess } from '.';
+import { ProjectProcess, TaskProcess, UserProcess } from '.';
 
 export class AppProcess {
   constructor() {}
@@ -8,7 +8,8 @@ export class AppProcess {
   async startAppProcess() {
     await new LogsUtil().addLogs('info', 'Start of an app process', 'app');
     await new ProjectProcess().startProjectProcess();
-
+    await new UserProcess().startUserProcess();
+    await new TaskProcess().startTaskProcess();
     await new LogsUtil().addLogs('info', 'End of an app process', 'app');
   }
 }
--- src/app/task-process.ts
+++ src/app/task-process.ts
@@ -1,3 +1,5 @@
 export class TaskProcess {
   constructor() {}
+
+  async startTaskProcess() {}
 }
