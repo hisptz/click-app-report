1b8b1c1dc8507a6d86234d7f09abc4816ee1a3eb
Joseph Chingalo <profschingalo@gmail.com>
2025-05-13 12:25:34 +0300

    feat: add support for the project extraction and report generations

--- src/app/app-process.ts
+++ src/app/app-process.ts
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import { LogsUtil } from '../utils';
+import { ProjectProcess } from '.';
 
 export class AppProcess {
   constructor() {}
 
   async startAppProcess() {
     await new LogsUtil().addLogs('info', 'Start of an app process', 'app');
-    /// process for scripts
+    await new ProjectProcess().startProjectProcess();
+
     await new LogsUtil().addLogs('info', 'End of an app process', 'app');
   }
 }
--- src/app/project-process.ts
+++ src/app/project-process.ts
@@ -1,3 +1,13 @@
+import { ApiProjectFolderModel } from '../models';
+import { ProjectUtil } from '../utils';
+
 export class ProjectProcess {
   constructor() {}
+
+  async startProjectProcess() {
+    const projectUtil = new ProjectUtil();
+    const projectFolderList: Array<ApiProjectFolderModel> =
+      await projectUtil.getProjectFolderList();
+    await projectUtil.generateProjectFolderListReport(projectFolderList);
+  }
 }
