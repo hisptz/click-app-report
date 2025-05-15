ed1c6264c135926a778ede41b310baf2ef4a080a
Joseph Chingalo <profschingalo@gmail.com>
2025-05-15 16:31:24 +0300

    feat: add support for dynamic generate dates for reports

--- src/admin-reports.ts
+++ src/admin-reports.ts
@@ -1,12 +1,15 @@
 import shelljs from 'shelljs';
 import { ADMIN_REPORT_TYPE, MINIMUM_WORKING_DAYS } from './constants';
+import { AppUtil } from './utils';
 
 starApp();
 
 async function starApp() {
-  //TODO dynamic dates
-  const startDate = '2025-05-01';
-  const endDate = '2025-05-31';
+  const monthIndex = new Date().getMonth();
+  const startDate = AppUtil.getFormattedDate(new Date(new Date().setDate(1)));
+  const endDate = AppUtil.getFormattedDate(
+    new Date(new Date(new Date().setMonth(monthIndex, 0)).setDate(1))
+  );
   const command = `./run-script.sh ${startDate} ${endDate} ${MINIMUM_WORKING_DAYS} ${ADMIN_REPORT_TYPE}`;
   shelljs.exec(command, (code, stdout, stderr) => {
     if (code !== 0) {
