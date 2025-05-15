3c5411f8677110bca346b39347fc5b7d2cb7d311
Joseph Chingalo <profschingalo@gmail.com>
2025-05-15 16:53:22 +0300

    fix: start and end date for timesheets

--- src/timesheets.ts
+++ src/timesheets.ts
@@ -1,14 +1,19 @@
 import shelljs from 'shelljs';
 import { MINIMUM_WORKING_DAYS, TIMESHEET_REPORT_TYPE } from './constants';
+import { AppUtil } from './utils';
 
 starApp();
 
 async function starApp() {
   const dayOfTimesheet = 22;
   const monthIndex = new Date().getMonth();
-  const startDate = new Date(new Date().setDate(dayOfTimesheet));
-  const endDate = new Date(
-    new Date(new Date().setMonth(monthIndex, 0)).setDate(dayOfTimesheet)
+  const startDate = AppUtil.getFormattedDate(
+    new Date(new Date().setDate(dayOfTimesheet))
+  );
+  const endDate = AppUtil.getFormattedDate(
+    new Date(
+      new Date(new Date().setMonth(monthIndex, 0)).setDate(dayOfTimesheet)
+    )
   );
   const command = `./run-script.sh ${startDate} ${endDate} ${MINIMUM_WORKING_DAYS} ${TIMESHEET_REPORT_TYPE}`;
   shelljs.exec(command, (code, stdout, stderr) => {
