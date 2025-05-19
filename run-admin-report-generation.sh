63ef5a49186d1336c7de08fad4e0d1ed96ced606
Joseph Chingalo <profschingalo@gmail.com>
2025-05-19 11:47:40 +0300

    chore: updating bash script for generating admin reports

--- run-admin-report-generation.sh
+++ run-admin-report-generation.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 clear
 npm run build-app
-#node --max_old_space_size=10000 dist/admin-reports.js
-
-if [ "$#" -eq "0" ]; then
-    node --max_old_space_size=10000 dist/admin-reports.js
-else
-    node --max_old_space_size=10000 dist/admin-reports.js $1 $2 $3 $4
-fi
+node --max_old_space_size=10000 dist/admin-reports.js
