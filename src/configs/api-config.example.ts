70afbbb1eb385945bc1d8aad51207b1abee04df0
Joseph Chingalo <profschingalo@gmail.com>
2025-08-20 12:26:28 +0300

    chore: add support for set selected projects as well as if for grouped timesheets

--- src/configs/api-config.example.ts
+++ src/configs/api-config.example.ts
@@ -3,7 +3,9 @@ import { ApiConfigModel, EmailConfigModel } from '../models';
 export const apiConfig: ApiConfigModel = {
   teamId: 'team-id-from-click-up',
   authorizationKey: 'click-up-generated-key',
-  appBaseUrl: 'https://api.clickup.com/api/v2'
+  appBaseUrl: 'https://api.clickup.com/api/v2',
+  isGroupedTimeSheets: false,
+  selectedProjects: []
 };
 
 export const emailConfig: EmailConfigModel = {
